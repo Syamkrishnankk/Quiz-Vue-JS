422fe94b4aaef67494451dd1d795851eb063ce99
Syam krishnan k k <86349818+Syamkrishnankk@users.noreply.github.com>
2024-07-15 16:19:55 +0530

    Update QuizCard.vue

--- src/components/QuizCard.vue
+++ src/components/QuizCard.vue
@@ -1,10 +1,10 @@
 <template>
     <v-card
-            class="py-4 mx-8 mt-8 w-20 px-5"
+            class="pt-6 mx-8 px-6"
             
             style="background-color: rgba(43, 43, 43, 0.533); color: rgba(240, 248, 255, 0.955);"
             rounded="lg"
-            variant="outlined"
+            variant="tonal"
           >
           <div class="pb-4">
             <slot name="question" ></slot>
